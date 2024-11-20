135d05a36c3fd484d3d0cfd99470aefc7f2b32e5
vishnu satam <vishnu.satam05@gmail.com>
2024-11-20 13:19:25 +0530

    add basic css propertis

---
+++ engine/features.py
@@ -0,0 +1,4 @@
+from playsound import playsound
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
--- www/main.js
+++ www/main.js
@@ -9,5 +9,31 @@ $(document).ready(function () {
             effect: "bounceOut",
         },
         
-    })
+    });
+
+    //siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+      });
+
+    //siri message enimation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true
+        },
+        out: {
+            effect: "fadeInOut",
+            sync: true
+        },
+        
+    });
 });
